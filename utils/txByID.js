6f3d52fcdbfd0393bb02fb6085f2a4907a4ef86a
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2024-08-12 14:55:52 +0400

    Update txByID.js

--- utils/txByID.js
+++ utils/txByID.js
@@ -24,23 +24,38 @@ const parseObjectDetails = async (object) => {
     console.log("Object not found.");
     return;
   }
+  object.op[1].fee.asset_id =
+    (await apiHelper.fetchAssetByName(object.op[1].fee.asset_id)).symbol ||
+    object.op[1].fee.asset_id;
+
   let from = (await getPublicKey(object.op[1].from)) || object.op[1].from;
   let to = (await getPublicKey(object.op[1].to)) || object.op[1].to;
 
   const obj = {
     amount: object.op[1].amount,
     fee: object.op[1].fee,
+    amount_to_reserve: object.op[1].amount_to_reserve,
     memo: object.op[1].memo,
     block_num: object.block_num,
     block_time: object.block_time,
   };
 
   if (object.op[0] == 15) {
+    object.op[1].amount_to_reserve.asset_id =
+      (
+        await apiHelper.fetchAssetByName(
+          object.op[1].amount_to_reserve.asset_id
+        )
+      ).symbol || object.op[1].amount_to_reserve.asset_id;
+
     let payer = object.op[1].payer;
     payer = await getPublicKey(payer);
 
     return { operation_type: object.op[0], id: object.id, payer, ...obj };
   } else {
+    object.op[1].amount.asset_id =
+      (await apiHelper.fetchAssetByName(object.op[1].amount.asset_id)).symbol ||
+      object.op[1].amount.asset_id;
     return {
       operation_type: object.op[0],
       id: object.id,
