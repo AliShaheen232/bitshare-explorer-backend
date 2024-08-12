fbc848f59e7c155e922bd04bfa49dd5750a42826
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2024-08-12 16:53:08 +0400

    Update pm2script.sh

--- pm2script.sh
+++ pm2script.sh
@@ -1,8 +1,10 @@
 pm2 kill
- fuser -k 3333/tcp
- 
+
+fuser -k 3333/tcp
+fuser -k 5500/tcp
+
 pm2 start ./server.js
-pm2 start ./scripts/switnessAndVotes.js
+pm2 start ./scripts/index.js
 pm2 start indexer/blockIndexer.js
 pm2 start indexer/transactionIndexer.js
 
