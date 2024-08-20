c32bf4166badb7765d04ed53c6414dc771a66f29
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2024-08-20 17:48:53 +0400

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://master:65rdeya6435%5E%23@rr-explorer-docdb-2024-08-20-11-58-55.cluster-crh7hz9fpnf9.eu-west-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_CONNECTION);
     console.log("MongoDB connected");
     return conn;
   } catch (error) {
